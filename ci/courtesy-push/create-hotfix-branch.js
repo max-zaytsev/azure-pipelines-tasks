7effad840c6bf31bf3046c0f87eb43c5c2e1502e
v-mazayt <v-mazayt@microsoft.com>
2023-04-27 13:54:22 +0200

    try

--- ci/courtesy-push/create-hotfix-branch.js
+++ ci/courtesy-push/create-hotfix-branch.js
@@ -4,10 +4,10 @@ const got = require('got');
 const cp = require('child_process');
 var util = require('../../make-util');
 
-const repository = process.env.REPOSITORY || "AzureDevOps";
-const taskName = process.env.TASK_NAME || "BashV3";
-const username = process.env.USERNAME || "v-mazayt";
-const sourcesDir = process.env['BUILD_SOURCESDIRECTORY'] || "/Users/krolikroger/Documents/git";
+const repository = process.env.REPOSITORY || 'AzureDevOps';
+const taskName = process.env.TASK_NAME || 'BashV3';
+const username = process.env.USERNAME || 'v-mazayt';
+const sourcesDir = process.env['BUILD_SOURCESDIRECTORY'] || '/Users/krolikroger/Documents/git';
 const GIT = 'git';
 
 /**
@@ -26,15 +26,12 @@ async function getCurrentSprint() {
   return sprint;
 }
 
-function commitChanges(directory, pathToAdd, branch, commitMessage) {
-  execInForeground(`${GIT} add ${pathToAdd}`, directory);
-  gitConfig();
-  execInForeground(`${GIT} checkout -b ${branch}`, directory);
-  execInForeground(`${GIT} commit -m "${commitMessage}" `, directory);
-  execInForeground(
-    `${GIT} -c credential.helper='!f() { echo "username=pat"; echo "password=$TOKEN"; };f' push --set-upstream origin ${branch}`,
-    directory
-  );
+function commitChanges(directory, pathToAdd, gitUrl, branch, commitMessage) {
+  // execInForeground(`${GIT} add ${pathToAdd}`, directory);
+  // gitConfig();
+  // execInForeground(`${GIT} checkout -b ${branch}`, directory);
+  // execInForeground(`${GIT} commit -m "${commitMessage}" `, directory);
+  execInForeground(`${GIT} push --set-upstream ${gitUrl} ${branch}`, directory);
 }
 
 function gitConfig() {
@@ -42,22 +39,21 @@ function gitConfig() {
   execInForeground(`${GIT} config --global user.name "${username}"`, null);
 }
 
-
 function execInForeground(command, directory, dryrun = false) {
   directory = directory || '.';
   console.log(`% ${command}`);
   if (!dryrun) {
     cp.execSync(command, { cwd: directory, stdio: [process.stdin, process.stdout, process.stderr] });
   }
-};
+}
 
 function copyHotFixFiles(hotfixFolderPath) {
   const src = path.join(sourcesDir, 'hotfixArtifact', 'hotfix');
   console.log(src);
-  const dest = path.join(sourcesDir, 'AzureDevOps.ConfigChange', hotfixFolderPath );
+  const dest = path.join(sourcesDir, 'AzureDevOps.ConfigChange', hotfixFolderPath);
   console.log(dest);
   var res = fs.existsSync(dest);
-  if(!fs.existsSync(dest)) {
+  if (!fs.existsSync(dest)) {
     fs.mkdirSync(dest);
   }
 
@@ -67,11 +63,12 @@ function copyHotFixFiles(hotfixFolderPath) {
 
 async function commitAzureDevOpsChanges(pathToAdoRepo, taskName) {
   const currentSprint = await getCurrentSprint();
-  const branch = `users/${username}/m${currentSprint}/${taskName}-UpdateUnifiedDeps-new5`;
+  const gitUrl = `https://${process.env.TOKEN}@dev.azure.com/v-mazayt0/AzureDevOps/_git/AzureDevOps`;
+  const branch = `users/${username}/m${currentSprint}/${taskName}-UpdateUnifiedDeps-new8`;
   console.log(`##vso[task.setVariable variable=hotFixBranch]${branch}`);
   const pathToUnifiedDependencies = path.join('.nuget', 'externals', 'UnifiedDependencies.xml');
   const commitMessage = `Update UnifiedDependencies.xml for ${taskName}`;
-  commitChanges(pathToAdoRepo, pathToUnifiedDependencies, branch, commitMessage);
+  commitChanges(pathToAdoRepo, pathToUnifiedDependencies, gitUrl, branch, commitMessage);
 }
 
 async function commitConfigChangeChanges(pathToCCRepo, taskName) {
@@ -90,8 +87,6 @@ async function commitConfigChangeChanges(pathToCCRepo, taskName) {
 function main() {
   console.log(sourcesDir + ' ' + repository);
   const pathToRepo = path.join(sourcesDir, repository);
-
-
 
   if (repository === 'AzureDevOps') {
     commitAzureDevOpsChanges(pathToRepo, taskName);
